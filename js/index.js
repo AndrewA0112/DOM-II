9f025c8a9c0ff5a8687512430fffc74d1d4a8a56
Andrew <andrew.mkal@outlook.com>
2019-06-18 11:26:43 -0500

    Added 10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,63 @@
 // Your code goes here
+
+
+let headerImg = document.querySelector('.intro img');
+headerImg.addEventListener('mouseover', headerImgMouseOver);
+headerImg.addEventListener('mouseleave', headerImgMouseLeave);
+
+function headerImgMouseOver (e) {
+    e.target.style.width = '50%';
+    e.target.style.margin = '0 auto';
+}
+
+function headerImgMouseLeave (e) {
+    e.target.style.width = '100%';
+}
+
+let headerMain = document.querySelector('.main-navigation');
+headerMain.addEventListener('mouseover', (event) =>
+{
+    event.target.style.backgroundColor = 'green';
+    event.stopPropagation();
+})
+headerMain.addEventListener('mouseleave', (event) =>
+{
+    event.target.style.backgroundColor = 'white';
+    event.stopPropagation();
+})
+
+let headerNav = document.querySelector('.nav-container');
+headerNav.addEventListener('mouseover', (event) => {
+    event.stopPropagation();
+})
+
+let mainContent = document.querySelector('body');
+mainContent.addEventListener('dblclick', () => {
+    alert('Hello!');
+})
+
+mainContent.addEventListener('wheel', (event) => {
+    alert('WEEEEEE');
+})
+
+
+
+let contentImg = document.querySelector('.content-section img')
+contentImg.addEventListener('drag', (event) =>
+{
+    event.target.style.display = 'none';
+})
+contentImg.addEventListener('dragend', (event) =>
+{
+    event.target.style.display = 'initial';
+})
+
+
+
+
+let footerText = document.querySelector('.footer p');
+footerText.addEventListener('mouseover', footerTextMouseOver)
+
+function footerTextMouseOver (e) {
+    e.target.textContent = 'Don\'t steal our stuff :(';
+}
